655f932211e1e8264b3bcbe6a0d3a15ab93ecfd2
Alysson Barros <arb35@yahoo.com.br>
2017-01-14 19:26:45 -0200

    Refazendo controle de versao

---
+++ alawebeventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,39 @@
+from django.core import mail
+from django.test import TestCase
+
+class SubscribePostValid(TestCase):
+	def setUp(self):
+		data = dict(name='Alysson Barros', cpf='12345678901',
+			email='alysson@barros.net', phone='21-99618-6180')
+
+		self.client.post('/inscricao/', data)
+		self.email = mail.outbox[0]
+
+	def test_subscription_email_subject(self):
+		expect = 'Confirmação de inscrição'
+
+		self.assertEqual(expect, self.email.subject)
+
+	def test_subscription_email_from(self):
+		expect = 'arbarros372@gmail.com'
+
+		self.assertEqual(expect, self.email.from_email)
+
+	def test_subscription_email_to(self):
+		expect = ['arbarros372@gmail.com', 'alysson@barros.net']
+
+		self.assertEqual(expect, self.email.to)
+
+	def test_subscription_email_body(self):
+		contents = [
+			'Alysson Barros',
+			'12345678901',
+			'alysson@barros.net',
+			'21-99618-6180',
+		]
+		for content in contents:
+			with self.subTest():
+				self.assertIn(content, self.email.body)
+
+
+		
